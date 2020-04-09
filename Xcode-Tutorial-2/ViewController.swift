9ea311f7e23d0167a3f5a4b585521d5f488f1839
Maverick <afaisal3389@gmail.com>
2020-04-09 07:07:24 +0530

    Initial Commit

---
+++ Xcode-Tutorial-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode-Tutorial-2
+//
+//  Created by Faisal Siddiqui on 09/04/20.
+//  Copyright © 2020 Faisal Siddiqui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
